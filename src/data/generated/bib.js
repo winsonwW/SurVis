934be58012eb9cd674ae6b69396da239deca65d0
winsonwW <132601215+winsonwW@users.noreply.github.com>
2023-05-13 22:32:34 +0100

    1

---
+++ src/data/generated/bib.js
@@ -0,0 +1,109 @@
+﻿define({ entries : {
+    "caroppo2020comparison": {
+        "author": "Caroppo, Andrea and Leone, Alessandro and Siciliano, Pietro",
+        "journal": "Journal of Computer Science and Technology",
+        "keywords": "type:computer vision, deep learning, facial expression, machine learning, ageing adult",
+        "pages": "1127--1146",
+        "publisher": "Springer",
+        "title": "Comparison between deep learning models and traditional machine learning approaches for facial expression recognition in ageing adults",
+        "type": "article",
+        "volume": "35",
+        "year": "2020"
+    },
+    "dubey2019review": {
+        "author": "Dubey, Arun Kumar and Jain, Vanita",
+        "journal": "Journal of Information and optimization sciences",
+        "keywords": "type:Deep Neural Network, 2D Face, 3D-Face, Data sets, Face Recognition",
+        "number": "2",
+        "pages": "547--558",
+        "publisher": "Taylor \\& Francis",
+        "title": "A review of face recognition methods using deep learning network",
+        "type": "article",
+        "volume": "40",
+        "year": "2019"
+    },
+    "ekundayo2021facial": {
+        "author": "Ekundayo, Olufisayo S and Viriri, Serestina",
+        "journal": "Ieee Access",
+        "keywords": "type:Facial expression recognition, single label learning, multilabel label learning, label distribution learning",
+        "pages": "136944--136973",
+        "publisher": "IEEE",
+        "title": "Facial expression recognition: a review of trends and techniques",
+        "type": "article",
+        "volume": "9",
+        "year": "2021"
+    },
+    "fathallah2017facial": {
+        "author": "Fathallah, Abir and Abdi, Lotfi and Douik, Ali",
+        "booktitle": "2017 IEEE/ACS 14th International Conference on Computer Systems and Applications (AICCSA)",
+        "keywords": "type:Facial Expression, Recognition, Deep Learning, CNN, Architecture, Classification",
+        "organization": "IEEE",
+        "pages": "745--750",
+        "title": "Facial expression recognition via deep learning",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "fathima2020review": {
+        "author": "Fathima, Amreen and Vaidehi, K",
+        "booktitle": "Advances in Decision Sciences, Image Processing, Security and Computer Vision: International Conference on Emerging Trends in Engineering (ICETE), Vol. 2",
+        "keywords": "type:Face detection, Facial expression recognition, Feature extraction, Classification",
+        "organization": "Springer",
+        "pages": "608--618",
+        "title": "Review on facial expression recognition system using machine learning techniques",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "harikrishnan2019vision": {
+        "author": "Harikrishnan, J and Sudarsan, Arya and Sadashiv, Aravind and Ajai, Remya AS",
+        "booktitle": "2019 international conference on vision towards emerging trends in communication and networking (ViTECoN)",
+        "keywords": "type:Face Attendance, Face Detection, Face Recognition, Computer Vision, Deep Learning, Biometric Surveillance",
+        "organization": "IEEE",
+        "pages": "1--5",
+        "title": "Vision-face recognition attendance monitoring system for surveillance using deep learning technology and computer vision",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "huang2019facial": {
+        "author": "Huang, Yunxin and Chen, Fei and Lv, Shaohe and Wang, Xiaodong",
+        "journal": "Symmetry",
+        "keywords": "type:facial expression recognition; feature extraction; classification; deep learning",
+        "number": "10",
+        "pages": "1189",
+        "publisher": "MDPI",
+        "title": "Facial expression recognition: A survey",
+        "type": "article",
+        "volume": "11",
+        "year": "2019"
+    },
+    "perveen2016facial": {
+        "author": "Perveen, Nazia and Ahmad, Nazir and Khan, MAQB and Khalid, Rizwan and Qadri, Salman",
+        "journal": "International Journal of Scientific \\& Technology Research",
+        "keywords": "type:CVIPtools, RST- Invariant, KNN, Human-Machine Interfaces",
+        "number": "03",
+        "title": "Facial expression recognition through machine learning",
+        "type": "article",
+        "volume": "5",
+        "year": "2016"
+    },
+    "wang2021single": {
+        "author": "Wang, Jie and Pan, Xiao and Li, Xinyu and Wei, Guangshun and Zhou, Yuanfeng",
+        "journal": "Graphics and Visual Computing",
+        "keywords": "type:Micro-expressions, Optical flow, Multi-scale, Recognition",
+        "pages": "200026",
+        "publisher": "Elsevier",
+        "title": "Single trunk multi-scale network for micro-expression recognition",
+        "type": "article",
+        "volume": "4",
+        "year": "2021"
+    },
+    "zhang2018facial": {
+        "author": "Zhang, Ting",
+        "booktitle": "Advances in Intelligent Systems and Interactive Applications: Proceedings of the 2nd International Conference on Intelligent and Interactive Systems and Applications (IISA2017)",
+        "keywords": "type:Facial expression,Deep learning, Feature extraction, Computer vision",
+        "organization": "Springer",
+        "pages": "345--352",
+        "title": "Facial expression recognition based on deep learning: a survey",
+        "type": "inproceedings",
+        "year": "2018"
+    }
+}});
